07befdbccb0577c4fe1416a4fc32ffbd44fc926d
Levente Szabadkai <levente@szabadkai.com>
2023-12-04 22:16:37 +0100

    better mob flip logic

--- Scripts/Orc.gd
+++ Scripts/Orc.gd
@@ -1,28 +1,29 @@
 extends CharacterBody2D
 var direction = 1 
 @onready var timer = $Timer
+@onready var ray_cast_2d = $RayCast2D
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.start()
+	set_up_direction(Vector2.UP)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	apply_gravity(delta)
 	apply_ai()
 	velocity.x = move_toward(velocity.x, 10 * direction, 200 * delta)
 	move_and_slide()
 
-
-	
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += 700 * delta
 
 func apply_ai():
-	if timer.time_left <= .01:
-		timer.start()
-		direction *= -1
-		$AnimatedSprite2D.scale.x = direction
+	if is_on_wall() or not ray_cast_2d.is_colliding():
+		flip()
+		
  
+func flip():
+	direction *= -1
+	scale.x *= -1
